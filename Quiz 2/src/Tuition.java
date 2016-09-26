939d82d55e4018a80b8118c61b826deaaae3f013
gregdorsey <gregdorsey@roaming-171-25.host.udel.edu>
2016-09-25 23:30:36 -0400

    Initial Check in

---
+++ Quiz 2/src/Tuition.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Tuition {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("What is the initial tuition: ");
+		int initial_tuition = input.nextInt();
+		
+		System.out.print("What is the percentage increase per year: ");
+		double percent_increase = input.nextDouble();
+		
+		float tuition = (float)initial_tuition;
+		float percentage = (float) (percent_increase/100);
+		float total = (float) 0;
+		for(int i = 0; i<4; i++){
+			tuition =  tuition*percentage + tuition;
+			total += tuition;
+			
+		}
+		
+		System.out.printf("The final cost for tuition after 4 years is $%.2f%n " , total);
+
+
+	}
+
+}
